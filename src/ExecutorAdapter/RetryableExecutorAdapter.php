7e58f3ee61c7c9a80c2a8a4cb497e5aecc142be2
Eugene Leonovich <eugene.leonovich@sanoma.com>
2016-09-29 11:45:06 +0200

    Tweak RetryableExecutorAdapter's error messages

--- src/ExecutorAdapter/RetryableExecutorAdapter.php
+++ src/ExecutorAdapter/RetryableExecutorAdapter.php
@@ -27,17 +27,17 @@ class RetryableExecutorAdapter implements ExecutorAdapter
         try {
             return $this->adapter->execute($payload->getPayload(), $context);
         } catch (ExecutionFailedException $e) {
-            $context->getLogger()->error('Task failed: '.$e->getMessage(), ['payload' => $payload]);
+            $context->getLogger()->error('Task failed: '.$e->getMessage(), ['payload' => $payload->getPayload()]);
         } catch (\Exception $e) {
             $delay = $this->retryStrategy->getDelay($payload->incRetry());
 
             if (null === $delay) {
-                $context->getLogger()->error('Task failed: '.$e->getMessage(), ['payload' => $payload]);
+                $context->getLogger()->error('Task failed: '.$e->getMessage(), ['payload' => $payload->getPayload()]);
 
                 return true;
             }
 
-            $context->getLogger()->error('An error occurred while executing the task: '.$e->getMessage(), ['payload' => $payload]);
+            $context->getLogger()->error('An error occurred while executing the task: '.$e->getMessage(), ['payload' => $payload->getPayload()]);
             $context->getQueue()->push($payload, "+$delay seconds");
         }
 
